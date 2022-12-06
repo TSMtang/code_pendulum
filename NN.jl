977d5928f395fe6ce079a5c3598ce1d86753de26
Shubham <shubham.sharma3297@gmail.com>
2022-12-06 19:58:53 +0100

    small change

--- NN.jl
+++ NN.jl
@@ -1,7 +1,11 @@
 using DataFrames
 using CSV
 using Flux
+using CUDA
 
 pendulum_output = CSV.read("C:\\Users\\Proteus\\Desktop\\RWTH\\WISE23\\Code_Pendulum\\CIE\\ProjectA\\code_pendulum\\pendulum.csv", DataFrame)
 
-print(pendulum_output.time)
+X = pendulum_output.time
+Y = pendulum_output.pos
+
+print(size(pendulum_output))
