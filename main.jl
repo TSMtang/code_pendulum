5a975b9bdbefee579a08ef8a403f5c643787f985
TSMtang <stang@modes.rwth-aachen.de>
2022-12-10 15:44:53 +0100

    central difference is done

--- main.jl
+++ main.jl
@@ -10,7 +10,6 @@
 using PrettyTables
 using Plots, Printf
 using DelimitedFiles
-using Flux
 
 ## booleans
 show_video = true
@@ -72,8 +71,62 @@ if show_video
     gif(anim_pendulum, "pendulum.gif")
 end
 
-
+## write data
+open("data.txt", "w") do io
+    writedlm(io, [x y1 y2])
+end
 
 ## Training a neural network
-# training dataset
-X = Integ.res_phi[1 : 0.35 * steps]
+using Flux, Statistics, ProgressMeter
+# prepare dataset
+training_points = round(Int, 0.35 * steps)
+
+x_train = range(1,training_points) * delta_t            |> collect
+x_test = range(training_points + 1, steps) * delta_t    |> collect
+y_train = Integ.res_phi[1 : training_points]
+y_test = Integ.res_phi[training_points + 1 : end]
+
+x_train = reshape(x_train, (1, length(x_train)))
+x_test = reshape(x_test, (1, length(x_test)))
+y_train = reshape(y_train, (1, length(y_train)))
+y_test = reshape(y_test, (1, length(y_test)))
+
+# model
+model = Chain(
+    Dense(1 => 5, tanh),   # activation function inside layer
+    BatchNorm(5),
+    Dense(5 => 5, tanh),
+    BatchNorm(5),
+    Dense(5 => 1),
+    )
+
+# huperparameters
+pars = Flux.params(model)  # contains references to arrays in model
+opt = Flux.Adam(0.01)      # will store optimiser momentum, etc.
+loader = Flux.DataLoader((x_train, y_train), batchsize=100, shuffle=true)
+
+# Training loop
+losses = []
+@showprogress for epoch in 1:1_000
+    for (x, y) in loader
+        loss, grad = Flux.withgradient(pars) do
+            # Evaluate model and loss inside gradient context:
+            y_hat = model(x)
+            Flux.mse(y_hat, y)
+        end
+        Flux.update!(opt, pars, grad)
+        push!(losses, loss)  # logging, outside gradient context
+    end
+end
+
+# predict and test
+x = hcat(x_train, x_test)
+y_predict = model(x)
+y_true = hcat(y_train, y_test)
+
+## plot results
+x = reshape(x, (length(x)))
+y_predict = reshape(y_predict, (length(y_predict)))
+y_true = reshape(y_true, (length(y_true)))
+plot(x, y_predict, xlabel = "time (s)", ylabel = "phi", label = "predict")
+plot!(x, y_true, xlabel = "time (s)", ylabel = "phi", label = "true")
